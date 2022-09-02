514cb327134a35449def43348464bd49b2c43f8e
alenushka <alionxlo@gmail.com>
2022-09-02 19:10:31 +0300

    Подключает скрипт выпадающего хедера

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,12 @@
-function captch() {
-  //тут что-то происходит
+const header = document.querySelector('.page-header');
+const burger = document.querySelector('.page-header__burger');
+
+function showHeader() {
+  header.classList.toggle('page-header--open');
 }
 
-if (captch) {
-  //если правда, то действие сделает это
-} else {
-  //иначе это
+function onOpenClick() {
+  showHeader();
 }
+
+burger.addEventListener('click', onOpenClick);
