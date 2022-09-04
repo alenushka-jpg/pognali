ed3bc86c6a3b12ddf9d8ce7a6480674e5c8104b1
alenushka <alionxlo@gmail.com>
2022-09-04 12:19:29 +0300

    Дорабатывает скрипт выпадающего меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,12 @@
 const header = document.querySelector('.page-header');
 const burger = document.querySelector('.page-header__burger');
+const body = document.querySelector('.page__body');
+
+function bodyHidden() {
+  if (body !== 'body') {
+    body.classList.toggle('page__body--hidden');
+  }
+}
 
 function showHeader() {
   header.classList.toggle('page-header--open');
@@ -7,6 +14,7 @@ function showHeader() {
 
 function onOpenClick() {
   showHeader();
+  bodyHidden();
 }
 
 burger.addEventListener('click', onOpenClick);
