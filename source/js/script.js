f7174fd7808d20c91c54907ea7b3f26b6a61d0f1
alenushka <alionxlo@gmail.com>
2022-09-04 12:41:17 +0300

    Подключает модальное окно Бизнес-тарифы

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,12 @@
 const header = document.querySelector('.page-header');
 const burger = document.querySelector('.page-header__burger');
 const body = document.querySelector('.page__body');
+const modalWindow = document.querySelector('.modal-window');
+const modalWindowClose = document.querySelector('.modal-window__button');
+const modalWindowOpen = document.querySelector('.add-profile__link');
 
-function bodyHidden() {
+//открытие хедера
+function bodyHiddenToggle() {
   if (body !== 'body') {
     body.classList.toggle('page__body--hidden');
   }
@@ -12,9 +16,39 @@ function showHeader() {
   header.classList.toggle('page-header--open');
 }
 
-function onOpenClick() {
+function onHeaderClick() {
   showHeader();
+  bodyHiddenToggle();
+}
+
+burger.addEventListener('click', onHeaderClick);
+
+//открытие модального окна Бизнес-тарифы
+function showModal() {
+  modalWindow.classList.add('modal-window--open');
+}
+
+function hiddenModal() {
+  modalWindow.classList.remove('modal-window--open');
+}
+
+function bodyHidden() {
+  body.classList.add('page__body--hidden');
+}
+
+function bodyShow() {
+  body.classList.remove('page__body--hidden');
+}
+
+function onOpenClick() {
+  showModal();
   bodyHidden();
 }
 
-burger.addEventListener('click', onOpenClick);
+function onCloseClick() {
+  hiddenModal();
+  bodyShow();
+}
+
+modalWindowOpen.addEventListener('click', onOpenClick);
+modalWindowClose.addEventListener('click', onCloseClick);
